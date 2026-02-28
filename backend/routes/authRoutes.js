9bbef822bdae470a4b9c2bf230019698acd62e06
Sukhpreet Singh <sukhpreetsingh3910a@gmail.com>
2026-02-28 19:07:22 +0530

    Addinge getMe function

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
-const {signup, login} = require('../controllers/authController');
+// const {signup, login} = require('../controllers/authController');
+const {signup, login, getMe} = require('../controllers/authController');
 const { protect } = require('../middleware/authMiddleware');
 
 router.post('/signup', signup);
